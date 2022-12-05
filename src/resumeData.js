d43e7a9db583ce251cd32647d072f3653a6ad83b
SATISH.V <sy4529630@gmail.com>
2022-12-05 15:11:35 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Amrutha",
+    "role": "Frontend Dev | Cybersec | React JS",
+    "linkedinId":"xyz@linkedin.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/amrutha-harshan-149aa2183",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Amruthaaaaa",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/amrutha_harshan?t=348mb5tR9Q6P20S3eGqiUw&s=09",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a Frontend web developer dabbed in for around 18 months, currently diving deeper into Machine Learning. Insert some cringe lines.I am eager to learn.I am determinedI never give up until I get something right. get on well with all kinds of people.I like to keep a positive attitude.(blah blah blah blah blah)",
+    "address":"India",
+    "website":"https://github.com/Amruthaaaaa",
+    "education":[
+      {
+        "UniversityName":"XYZ COllege",
+        "specialization":"BS specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        "Achievements":"BS Achievements"
+      },
+      {
+        "UniversityName":"blah University",
+        "specialization":"blah specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"1618",
+        "Achievements":"blah blah Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"1788",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Blah Company",
+        "specialization":"Blah specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"1898",
+        "Achievements":"Blah Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5/CSS"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical geek"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical geek"
+      }
+    ]
+  }
+  
+  export default resumeData
